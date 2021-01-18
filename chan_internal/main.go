142c7a7d9d9ede6a7e3aba86f5c08567c99df616
Konstantin Voykov <koykov0@gmail.com>
2021-01-18 23:50:23 +0200

    Check if channel is closed by internals.

---
+++ chan_internal/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	var ch chan interface{}
+	ch = make(chan interface{}, 10)
+	chcptr := chClosePtr(ch)
+	fmt.Println("closed", *chcptr)
+	ch <- 1
+	ch <- "foo"
+	fmt.Println("closed", *chcptr)
+	close(ch)
+	fmt.Println("closed", *chcptr)
+}
+
+func chClosePtr(ch interface{}) *uint32 {
+	chptr := *(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&ch)) + unsafe.Sizeof(uint(0))))
+	chptr += unsafe.Sizeof(uint(0)) * 2
+	chptr += unsafe.Sizeof(0)
+	chptr += unsafe.Sizeof(uint16(0))
+	return (*uint32)(unsafe.Pointer(chptr))
+}
