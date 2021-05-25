8440982627579c9b343508b0025bd895f90dbac2
Konstantin Voykov <koykov0@gmail.com>
2021-05-25 12:21:37 +0300

    Loop vs goto performance comparison.

---
+++ loop_goto/main_test.go
@@ -0,0 +1,38 @@
+package loop_goto
+
+import "testing"
+
+func BenchmarkLoop(b *testing.B) {
+	a := 0
+	for i := 0; i < b.N; i++ {
+		a = floop(50)
+	}
+	_ = a
+}
+
+func BenchmarkGoto(b *testing.B) {
+	a := 0
+	for i := 0; i < b.N; i++ {
+		a = fgoto(50)
+	}
+	_ = a
+}
+
+func floop(size int) int {
+	f := 1
+	for i := 1; i <= size; i++ {
+		f *= i
+	}
+	return f
+}
+
+func fgoto(size int) int {
+	i, f := 0, 1
+loop:
+	i++
+	f *= i
+	if i < size {
+		goto loop
+	}
+	return f
+}
