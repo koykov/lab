4de606c86694c04b8a2aa0a4efb8a57d4a6518b4
Konstantin Voykov <koykov0@gmail.com>
2021-05-31 17:51:50 +0300

    Bounds check off performance issue.

---
+++ bounds_check/main_test.go
@@ -0,0 +1,38 @@
+package bounds_check
+
+import "testing"
+
+const limit = 1e3
+
+var (
+	a = make([]int32, limit)
+)
+
+func init() {
+	for i := 0; i < limit; i++ {
+		a[i] = int32(i)
+	}
+}
+
+func BenchmarkLoopBoundsCheckOn(b *testing.B) {
+	b.ResetTimer()
+	var x int32
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < limit; j++ {
+			x += a[j]
+		}
+	}
+	b.Log(x)
+}
+
+func BenchmarkLoopBoundsCheckOff(b *testing.B) {
+	b.ResetTimer()
+	var x int32
+	for i := 0; i < b.N; i++ {
+		_ = a[limit-1]
+		for j := 0; j < limit; j++ {
+			x += a[j]
+		}
+	}
+	b.Log(x)
+}
