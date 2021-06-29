3fd66745ce928f76a163b14d34b8ea451e36909b
Konstantin Voykov <koykov0@gmail.com>
2021-06-29 22:53:21 +0300

    Test logger with input buffer.

--- logger/main_test.go
+++ logger/main_test.go
@@ -15,6 +15,7 @@ func (l *Log) Printf(string, ...interface{}) {}
 var (
 	li Logger
 	ld Log
+	a  []interface{}
 )
 
 func BenchmarkLoggerIface_Print(b *testing.B) {
@@ -22,7 +23,7 @@ func BenchmarkLoggerIface_Print(b *testing.B) {
 	li = &Log{}
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		li.Printf("x", x, "y", y, "z", z)
+		li.Printf("", x, y, z)
 	}
 }
 
@@ -30,6 +31,25 @@ func BenchmarkLoggerDirect_Print(b *testing.B) {
 	x, y, z := 1, "foo", "bar"
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		ld.Printf("x", x, "y", y, "z", z)
+		ld.Printf("", x, y, z)
+	}
+}
+
+func BenchmarkLoggerBufIface_Print(b *testing.B) {
+	x, y, z := 1, "foo", "bar"
+	li = &Log{}
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		a = append(a[:0], x, y, z)
+		li.Printf("", a...)
+	}
+}
+
+func BenchmarkLoggerBufDirect_Print(b *testing.B) {
+	x, y, z := 1, "foo", "bar"
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		a = append(a[:0], x, y, z)
+		ld.Printf("", a...)
 	}
 }
