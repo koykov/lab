7281c28e9e543ea8cc8cbb5d784c7955d0964e7a
Konstantin Voykov <koykov0@gmail.com>
2021-07-19 23:10:44 +0300

    Check nested map access performance.

---
+++ mapaccess/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import "testing"
+
+type mapL1 map[int32]struct{}
+type mapL2 map[int32]map[int32]struct{}
+type mapL3 map[int32]map[int32]map[int32]struct{}
+
+var (
+	l1 = mapL1{1: struct{}{}, 2: struct{}{}, 3: struct{}{}}
+	l2 = mapL2{
+		1: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+		2: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+		3: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+	}
+	l3 = mapL3{
+		1: {
+			1: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			2: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			3: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+		},
+		2: {
+			1: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			2: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			3: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+		},
+		3: {
+			1: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			2: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			3: {1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+		},
+	}
+)
+
+func BenchmarkAccessL1(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		if _, ok := l1[2]; !ok {
+			b.Error("l1: not found")
+		}
+	}
+}
+
+func BenchmarkAccessL2(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		if _, ok := l2[2][2]; !ok {
+			b.Error("l2: not found")
+		}
+	}
+}
+
+func BenchmarkAccessL3(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		if _, ok := l3[2][2][2]; !ok {
+			b.Error("l3: not found")
+		}
+	}
+}
