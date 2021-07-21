aa96b3693a72886ca11425391f17603de7140561
Konstantin Voykov <koykov0@gmail.com>
2021-07-21 22:32:36 +0300

    Check time.Now() versus singe atomic ticker performance.

---
+++ time/now_test.go
@@ -0,0 +1,37 @@
+package time
+
+import (
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+var (
+	now uint32
+)
+
+func init() {
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				atomic.StoreUint32(&now, uint32(time.Now().Unix()))
+			}
+		}
+	}()
+}
+
+func BenchmarkTimeNowNative(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x := time.Now()
+		_ = x
+	}
+}
+
+func BenchmarkTimeNowAtomic(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x := atomic.LoadUint32(&now)
+		_ = x
+	}
+}
