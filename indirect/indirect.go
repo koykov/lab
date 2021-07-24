b2157bdb5fac10c3762c877447c372536b759084
Konstantin Voykov <koykov0@gmail.com>
2021-07-24 22:45:32 +0300

    Comment issue: indirect test.

--- indirect/indirect.go
+++ indirect/indirect.go
@@ -2,6 +2,7 @@ package indirect
 
 import "unsafe"
 
+// Indirect uint32 value from uintptr pointer using unsafe.Pointer. go vet warning about possible misuse of unsafe.Pointer.
 func uptrIndirNative(addr uintptr) uint32 {
 	return *(*uint32)(unsafe.Pointer(addr))
 }
--- indirect/indirect_amd64.go
+++ indirect/indirect_amd64.go
@@ -4,5 +4,8 @@
 
 package indirect
 
+// Indirect uint32 value from uintptr pointer, avoiding using unsafe.Pointer and as a result without unsafe.Pointer
+// misuse warning.
+//
 //go:noescape
 func uptrIndirAsm(addr uintptr) uint32
