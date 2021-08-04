606b7a6b4079c359651dcde4c0fc62c0e47995c5
Konstantin Voykov <koykov0@gmail.com>
2021-08-04 21:30:50 +0300

    Command queue implementation.

---
+++ cmd_queue/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	bucketsSize    = 64
+	bucketsWorkers = 4
+)
+
+var (
+	buckets = make([]int, bucketsSize)
+)
+
+func init() {
+	for i := 0; i < bucketsSize; i++ {
+		buckets[i] = i
+	}
+}
+
+func main() {
+	queue := make(chan int, bucketsWorkers)
+
+	var (
+		wg   sync.WaitGroup
+		proc uint32
+	)
+	for i := 0; i < bucketsWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				idx, ok := <-queue
+				if !ok {
+					break
+				}
+
+				time.Sleep(time.Duration(200+rand.Int31n(200)) * time.Millisecond)
+				processed := atomic.AddUint32(&proc, 1)
+				fmt.Println(processed, idx)
+			}
+		}()
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < bucketsSize; i++ {
+			queue <- i
+		}
+		close(queue)
+	}()
+
+	wg.Wait()
+}
