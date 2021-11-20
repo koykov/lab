137ce903b815ac6d30eed40ec50db0d2c469a120
Konstantin Voykov <koykov0@gmail.com>
2021-11-20 23:48:39 +0200

    Clock project: cover project with benchmark tests; add readme.

--- clock/clock_test.go
+++ clock/clock_test.go
@@ -7,12 +7,32 @@ import (
 
 func TestClock(t *testing.T) {
 	t.Run("now", func(t *testing.T) {
-		c := clock{delta: time.Millisecond}
+		c := clock{delta: time.Microsecond}
 		c.Start()
-		for i := 0; i < 500; i++ {
+		for i := 0; i < 50; i++ {
 			t.Log(time.Now().Sub(c.Now()))
 			time.Sleep(time.Millisecond)
 		}
 		c.Stop()
+	})
+}
+
+func BenchmarkClock(b *testing.B) {
+	b.Run("now clock", func(b *testing.B) {
+		c := clock{delta: time.Microsecond}
+		c.Start()
+		var n time.Time
+		for i := 0; i < b.N; i++ {
+			n = c.Now()
+		}
+		_ = n
+		c.Stop()
+	})
+	b.Run("now native", func(b *testing.B) {
+		var n time.Time
+		for i := 0; i < b.N; i++ {
+			n = time.Now()
+		}
+		_ = n
 	})
 }
---
+++ clock/readme.md
@@ -0,0 +1,4 @@
+```
+BenchmarkClock/now_clock-8         	1000000000	         0.8297 ns/op
+BenchmarkClock/now_native-8        	20708202	         57.81 ns/op
+```
