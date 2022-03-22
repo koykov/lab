55b50237b1256c66d0bd6e2e83c28c7bbbcc333a
Konstantin Voykov <koykov0@gmail.com>
2022-03-22 10:26:16 +0200

    Swap vars project: add division/multiplication algo.

--- swap_var/swap_var_test.go
+++ swap_var/swap_var_test.go
@@ -25,6 +25,17 @@ func BenchmarkSwapVar(b *testing.B) {
 			}
 		}
 	})
+	b.Run("divmul", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			x, y := 5, 17
+			x = x * y
+			y = x / y
+			x = x / y
+			if x != 17 || y != 5 {
+				b.FailNow()
+			}
+		}
+	})
 	b.Run("sugar", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			x, y := 5, 17
