706d0ba18b75cf865fe90e18816d4c857849e703
Konstantin Voykov <koykov0@gmail.com>
2022-03-22 10:19:19 +0200

    Swap vars performance comparison project.

---
+++ swap_var/swap_var_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func BenchmarkSwapVar(b *testing.B) {
+	b.Run("delta", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			x, y := 5, 17
+			x = x + y
+			y = x - y
+			x = x - y
+			if x != 17 || y != 5 {
+				b.FailNow()
+			}
+		}
+	})
+	b.Run("xor", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			x, y := 5, 17
+			x ^= y
+			y ^= x
+			x ^= y
+			if x != 17 || y != 5 {
+				b.FailNow()
+			}
+		}
+	})
+	b.Run("sugar", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			x, y := 5, 17
+			x, y = y, x
+			if x != 17 || y != 5 {
+				b.FailNow()
+			}
+		}
+	})
+}
