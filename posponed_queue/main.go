71606de15b39312a43104ea3e149f1751a36669f
Konstantin Voykov <koykov0@gmail.com>
2022-08-03 18:18:38 +0300

    pequeue project: graceful shutdown.

--- posponed_queue/main.go
+++ posponed_queue/main.go
@@ -15,6 +15,7 @@ const (
 type pq struct {
 	d time.Duration
 	q chan item
+	e chan struct{}
 }
 
 type item struct {
@@ -27,6 +28,15 @@ var (
 	c  uint32
 )
 
+func newq() *pq {
+	q := &pq{
+		d: time.Second * 10,
+		q: make(chan item, size),
+		e: make(chan struct{}, size),
+	}
+	return q
+}
+
 func (q *pq) enqueue(x any) {
 	t := time.Now()
 	q.q <- item{
@@ -36,7 +46,8 @@ func (q *pq) enqueue(x any) {
 	fmt.Printf("enqueue %d at %s\n", x, t.Format(time.RFC3339Nano))
 }
 
-func (q *pq) do(fn func(any) error) {
+func (q *pq) do(wg *sync.WaitGroup, fn func(any) error) {
+	defer wg.Done()
 	for {
 		select {
 		case itm := <-q.q:
@@ -46,22 +57,26 @@ func (q *pq) do(fn func(any) error) {
 				time.Sleep(d)
 			}
 			_ = fn(itm.pl)
+		case <-q.e:
+			return
 		}
 	}
 }
 
+func (q *pq) close() {
+	close(q.q)
+	close(q.e)
+}
+
 func init() {
-	qi = &pq{
-		d: time.Second * 10,
-		q: make(chan item, size),
-	}
+	qi = newq()
 }
 
 func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < wc; i++ {
 		wg.Add(1)
-		go qi.do(func(x any) error {
+		go qi.do(&wg, func(x any) error {
 			fmt.Printf("process %d at %s\n", x, time.Now().Format(time.RFC3339Nano))
 			time.Sleep(time.Millisecond * 67)
 			return nil
@@ -80,5 +95,10 @@ func main() {
 	}
 	time.Sleep(time.Second * 20)
 
+	for i := 0; i < wc; i++ {
+		qi.e <- struct{}{}
+	}
 	wg.Wait()
+
+	qi.close()
 }
