34d2ace37ea37da9c5bb41c007ed4c63249f94f9
Konstantin Voykov <koykov0@gmail.com>
2025-08-18 23:00:28 +0300

    chan_internal project

--- chan_internal/main.go
+++ chan_internal/main.go
@@ -1,62 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
-	"unsafe"
+	"context"
+	"sync"
 )
 
-type stream struct {
-	data  chan interface{}
-	clbuf []byte
-}
-
-func newStream(size uint64) *stream {
-	s := &stream{}
-	s.data = make(chan interface{}, size)
-	var i interface{}
-	i = s.data
-
-	chptr := *(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&i)) + unsafe.Sizeof(uint(0))))
-	fmt.Println("m ptr", chptr)
-	chptr += unsafe.Sizeof(uint(0)) * 2
-	chptr += unsafe.Sizeof(0)
-	chptr += unsafe.Sizeof(uint16(0))
-	h := reflect.SliceHeader{
-		Data: chptr,
-		Len:  4,
-		Cap:  4,
-	}
-	s.clbuf = *(*[]byte)(unsafe.Pointer(&h))
-
-	return s
-}
-
-func (s *stream) push(x interface{}) {
-	s.data <- x
-}
-
-func (s *stream) pull() interface{} {
-	return <-s.data
-}
-
-func (s *stream) closed() bool {
-	if len(s.clbuf) == 0 {
-		return true
-	}
-	return s.clbuf[2] == 1
-}
-
-func (s *stream) close() {
-	close(s.data)
-}
-
 func main() {
-	ch := newStream(100000)
-	fmt.Println(ch.closed())
-	ch.push(1)
-	ch.push("foo")
-	fmt.Println(ch.closed())
-	ch.close()
-	fmt.Println(ch.closed())
+	ctx, cancel := context.WithCancel(context.Background())
+
+	c := make(chan int)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(ctx context.Context) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					c <- 1 // heizenpanic
+				}
+			}
+		}(ctx)
+	}
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				close(c)
+				return
+			case x := <-c:
+				_ = x
+			}
+		}
+	}(ctx)
+
+	cancel()
+
+	wg.Wait()
 }
